8f02d6299a90ed69f9f22044835412f24c6227f7
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-08-30 01:28:50 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Jethro Fredericks</p>
+    <p>I'm a developer-in-training from South Africa</p>
+    <p>I aspire to be a fullstack developer using the MERN stack</p>
+  </div>
+)
+
+export default IndexPage
